b021606f04157fc050697397ef55fa30e062ee2b
Leandro Gabriel <leogabrields97@gmail.com>
2023-08-07 17:26:47 -0300

    ensure HttpAdapter calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -8,33 +10,49 @@ class HttpAdapter {
   HttpAdapter(
     this.client,
   );
-  Future<void> request(Uri url, String method) async {
+  Future<void> request({
+    required url,
+    required String method,
+    String? body,
+  }) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
 class ClientSpy extends Mock implements Client {}
 
 void main() {
+  final client = ClientSpy();
+  final sut = HttpAdapter(client);
+  final url = Uri.parse(faker.internet.httpUrl());
+  final headers = {
+    'content-type': 'application/json',
+    'accept': 'application/json'
+  };
+  const body = '{"any_key":"any_value"}';
+
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = Uri.parse(faker.internet.httpUrl());
-      final headers = {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      };
-
-      when(() => client.post(url, headers: headers))
+      when(() => client.post(url, headers: headers, body: jsonEncode(body)))
           .thenAnswer((_) async => Response('{}', 200));
-      await sut.request(url, 'post');
 
-      verify(() => client.post(url, headers: headers));
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: body,
+      );
+
+      verify(
+        () => client.post(
+          url,
+          headers: headers,
+          body: jsonEncode(body),
+        ),
+      );
     });
   });
 }
